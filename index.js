d57f7d27f95d45e5057262ad23894ba65069008f
al-mojtaba <138788931+al-mojtaba@users.noreply.github.com>
2024-12-14 10:33:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+puppeteer.use(StealthPlugin());
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -6,9 +9,20 @@ import puppeteer from "puppeteer";
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
+  console.log("Running tests..");
   const page = await browser.newPage();
-  await page.goto("https://example.com");
+
+  await page.goto(
+    "https://www.bybit.com/copyTrade/trade-center/detail?leaderMark=DLlcAmuhyFKi1WG5zqIIug%3D%3D&copyFrom=CTList",
+    { timeout: 0 }
+  );
+
+  const element = await page.$('div[data-orientation="horizontal"] span div');
+  await element.click();
+
   console.log(await page.title());
+
+  console.log(`All done ✨`);
 
   await browser.close();
 })();
